1ef22dc4b298c2a937e14f17b8f659b3b5bf2a84
nz <>
2025-04-01 23:17:03 +0300

    some sketches
    performed basic go project management
    
    added few files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nunzayin/nzkeys
+
+go 1.24.1
---
+++ internal/help.go
@@ -0,0 +1,6 @@
+package ints
+
+const HELP_TEXT = `
+Usage: nzkeys COMMAND [ARGUMENTS]...
+Manage your accounts and passwords via CLI.
+`
